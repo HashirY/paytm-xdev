8d55a0395b0d16870f1bd4740ba6e76a9ad88b7b
Hashir <hashir0601.be21@chitkara.edu.in>
2024-02-04 17:15:54 +0530

    account schema created

--- backend/db.js
+++ backend/db.js
@@ -1,41 +1,58 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://root:root@100xdev.kmbpv1b.mongodb.net/paytm-clone?retryWrites=true&w=majority").then(() => {
-  console.log("DB connected")
-})
+mongoose
+  .connect(
+    "mongodb+srv://root:root@100xdev.kmbpv1b.mongodb.net/paytm-clone?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("DB connected");
+  });
 
 const userSchema = new mongoose.Schema({
-
-  username:{
-    type:String,
-    required:true,
-    unique:true,
-    trim:true,
-    lowercase:true,
-    minLength:3,
-    maxLength:30
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
   },
-  password:{
-    type:String,
-    required:true,
-    minLength:6
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
   },
-  firstName:{
-    type:String,
-    required:true,
-    trim:true,
-    maxLength:50
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
   },
-  lastName:{
-    type:String,
-    required:true,
-    trim:true,
-    maxLength:50
-  }
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
 });
 
-const User = mongoose.model("User" , userSchema);
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+const Account = mongoose.model("Account", accountSchema);
 
 module.exports = {
-  User
-}
+  User,
+  Account,
+};
